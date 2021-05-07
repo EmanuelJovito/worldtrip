d69e3365268321d5a134337f95a832ed456152aa
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-05-07 14:57:18 -0300

    feat: Configurando swiper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
+// Slider config
+import SwiperCore, { Navigation,Pagination,Mousewheel,Keyboard } from 'swiper/core';
+
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css"
+import "swiper/components/pagination/pagination.min.css"
+
+SwiperCore.use([Navigation,Pagination,Mousewheel,Keyboard]);
+//
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
