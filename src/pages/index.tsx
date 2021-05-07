fd09917eb161fb7e3e8c12e16e6b49e6a64a0092
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-05-07 14:56:57 -0300

    refactor: Componentizando a homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,29 @@
-import { Flex, Image, Text } from '@chakra-ui/react'
+import { Flex, Text } from '@chakra-ui/react'
+
+import { Banner } from '../components/Banner'
+import { ContinentsSlide } from '../components/ContinentsSlide/ContinentsSlide'
+import { Header } from '../components/Header'
+import { TravelTypes } from '../components/TravelTypes'
 
 export default function Home() {
   return (
     <>
       <Flex
         w="100vw" 
-        h="100vh" 
+        h="100%" 
         flexDirection="column"
+        align="center"
       >
-        <Flex
-          w="100vw"
-          h="3.5rem"
-          align="center"
-          justify="center"
-        >
-          <Image src="logo.svg" boxSize="9rem"/>
-        </Flex>
-        <Flex
-          w="100vw"
-          h="18rem"
-        >
-          <Image 
-            src="background.png" 
-            w="100vw" 
-            h="15rem"/>
-          <Image 
-            src="airplane.svg" 
-            boxSize="18rem" 
-            position="absolute"
-            right="20"
-            top="75"
-          />
-          <Text 
-            position="absolute" 
-            color="#F5F8FA" 
-            fontWeight="500"
-            fontSize="1.5rem"
-            left="20"
-            top="115"
-          >
-            5 Continentes,<br/>
-            infinitas possibilidades.
-          </Text>
-          <Text
-            position="absolute"
-            fontWeight="400"
-            color="#DADADA"
-            left="20"
-            top="200"
-          >
-            Chegou a hora de tirar do papel a viagem que você<br/> sempre sonhou. 
-          </Text>
-        </Flex>
+        <Header />
+        <Banner />
+        <TravelTypes />
+
+        <Text textAlign="center" fontWeight="500" fontSize="2rem" marginTop="2rem" mb="2rem">
+        Vamos nessa?<br/>
+        Então escolha seu continente
+        </Text>
+
+        <ContinentsSlide />
       </Flex>
     </>
   )
