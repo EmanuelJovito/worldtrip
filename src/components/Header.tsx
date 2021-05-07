7b06e7d48800076681c34f1c649200bf41616de1
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-05-07 11:24:46 -0300

    feat: Criando componente Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { Flex, Image } from "@chakra-ui/react";
+
+export function Header() {
+  return (
+    <Flex
+      w="100vw"
+      h="3.5rem"
+      align="center"
+      justify="center"
+    >
+      <Image src="logo.svg" boxSize="9rem"/>
+    </Flex>
+  )
+}
