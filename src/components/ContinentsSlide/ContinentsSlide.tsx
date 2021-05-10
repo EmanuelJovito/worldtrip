99706d994bc945986c7da16f422029973e27bb7f
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-05-10 11:40:48 -0300

    refactor: Mudança nas imagens

--- src/components/ContinentsSlide/ContinentsSlide.tsx
+++ src/components/ContinentsSlide/ContinentsSlide.tsx
@@ -15,28 +15,28 @@ export function ContinentsSlide() {
         className={styles.myswaper}
       >
         <SwiperSlide className={styles.swiperSlide}>
-          <Image src="/europa.png" className={styles.swiperImage}/>
+          <Image src="continentes/europa.png" className={styles.swiperImage}/>
           <Text fontWeight="700" position="absolute" fontSize="3rem" color="#DADADA">Europa</Text>
           <Text fontWeight="700" position="absolute" top="250" fontSize="1.5rem" color="#DADADA">O continente mais antigo.</Text>
         </SwiperSlide>
         <SwiperSlide className={styles.swiperSlide}>
-          <Image src="/americadosul.jpg" className={styles.swiperImage}/>
+          <Image src="continentes/americadosul.jpg" className={styles.swiperImage}/>
           <Text fontWeight="700" position="absolute" fontSize="3rem" color="white">América do Sul</Text>
         </SwiperSlide>
         <SwiperSlide className={styles.swiperSlide}>
-          <Image src="/americadonorte.jpg" className={styles.swiperImage}/>
+          <Image src="continentes/americadonorte.jpg" className={styles.swiperImage}/>
           <Text fontWeight="700" position="absolute" fontSize="3rem" color="white">América do Norte</Text>
         </SwiperSlide>
         <SwiperSlide className={styles.swiperSlide}>
-          <Image src="/asia.jpg" className={styles.swiperImage}/>
+          <Image src="continentes/asia.jpg" className={styles.swiperImage}/>
           <Text fontWeight="700" position="absolute" fontSize="3rem" color="white">Ásia</Text>
         </SwiperSlide>
         <SwiperSlide className={styles.swiperSlide}>
-          <Image src="/africa.jpg" className={styles.swiperImage}/>
+          <Image src="continentes/africa.jpg" className={styles.swiperImage}/>
           <Text fontWeight="700" position="absolute" fontSize="3rem" color="white">África</Text>
         </SwiperSlide>
         <SwiperSlide className={styles.swiperSlide}>
-          <Image src="/oceania.jpg" className={styles.swiperImage}/>
+          <Image src="continentes/oceania.jpg" className={styles.swiperImage}/>
           <Text fontWeight="700" position="absolute" fontSize="3rem" color="white">Oceania</Text>
         </SwiperSlide>
       </Swiper>
