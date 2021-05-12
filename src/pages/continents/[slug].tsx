a607beb96467adba5f9763a8a81a0822c2fab2cb
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-05-12 14:36:25 -0300

    feat: Transformando em pagina estatica

--- src/pages/continents/[slug].tsx
+++ src/pages/continents/[slug].tsx
@@ -1,10 +1,29 @@
 import { Box, Flex, Image } from "@chakra-ui/react";
+import { GetStaticProps } from "next";
 import { BannerEuropa } from "../../components/BannerEuropa";
 import { Cidades100 } from "../../components/Cidades100";
 import { EuropaInfo } from "../../components/EurpaInfo";
 import { Header } from "../../components/Header";
+import { api } from "../../services/api";
 
-export default function Europa() {
+type Continent = {
+  id: string,
+  continent: string,
+  description: string,
+  countries: string,
+  language: string,
+  citys: string,
+  countrie: string,
+  city: string,
+  thumb: string,
+  flag: string,
+}
+
+type ContinentProps = {
+  continent: Continent
+}
+
+export default function Continents({continent}: ContinentProps) {
   return (
     <>
       <Flex
@@ -13,6 +32,7 @@ export default function Europa() {
         flexDirection="column" 
         align="center"
       >
+        {}
         <Header />
         <Box as="a" href="/" position="absolute" left="100" top="5">
           <Image src="/voltar.svg"/>
@@ -23,4 +43,29 @@ export default function Europa() {
       </Flex>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { slug } = ctx.params
+
+  const { data } = await api.get(`/continents/${slug}`)
+
+  const continents = {
+    id: data.id,
+    continent: data.continent,
+    description: data.description,
+    countries: data.countries,
+    language: data.language,
+    citys: data.citys,
+    countrie: data.countrie,
+    city: data.city,
+    thumb: data.thumb,
+    flag: data.flag
+  }
+
+  return {
+    props: {
+      continents
+    }
+  }
 }
