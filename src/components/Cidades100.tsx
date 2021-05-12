3e02d5e4f4b2e0c82af3651347b3204399de68c0
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-05-12 10:24:38 -0300

    refactor: Mudando o border dos grids

--- src/components/Cidades100.tsx
+++ src/components/Cidades100.tsx
@@ -10,50 +10,80 @@ export function Cidades100() {
     </Flex>
 
     <Grid mt="2rem" gridTemplateColumns="repeat(4, 1fr)" rowGap="1rem" columnGap="1rem" mb="2rem">
-      <Box border="1px solid #FFBA08" borderRadius="5px">
+      <Box>
         <Image src="cidades/londres.png" w="100%"/>
-        <Flex mt="1rem" mb="1rem" justify="space-between">
-          <Flex flexDirection="column" ml="1rem">
+        <Flex 
+          justify="space-between" 
+          borderRight="1px solid #FFBA08" 
+          borderBottom="1px solid #FFBA08" 
+          borderLeft="1px solid #FFBA08" 
+          borderRadius="0 0 5px 5px"
+        >
+          <Flex flexDirection="column" ml="1rem" mt="1rem" mb="1rem">
             <Text fontWeight="600" fontSize="1.25rem">Londres</Text>
             <Text mt="0.5rem">Reino Unido</Text>
           </Flex>
           <Image src="bandeiras/reinounido.svg" mr="1rem"/>
         </Flex>
       </Box>
-      <Box border="1px solid #FFBA08" borderRadius="5px">
+      <Box>
         <Image src="cidades/paris.png" w="100%"/>
-        <Flex mt="1rem" mb="1rem"  justify="space-between">
-          <Flex flexDirection="column" ml="1rem">
+        <Flex 
+          justify="space-between" 
+          borderRight="1px solid #FFBA08" 
+          borderBottom="1px solid #FFBA08" 
+          borderLeft="1px solid #FFBA08" 
+          borderRadius="0 0 5px 5px"
+        >
+          <Flex flexDirection="column" ml="1rem" mt="1rem" mb="1rem">
             <Text fontWeight="600" fontSize="1.25rem">Paris</Text>
             <Text mt="0.5rem">França</Text>
           </Flex>
           <Image src="bandeiras/franca.svg" mr="1rem"/>
         </Flex>
       </Box>
-      <Box border="1px solid #FFBA08" borderRadius="5px">
+      <Box>
         <Image src="cidades/roma.png" w="100%"/>
-        <Flex mt="1rem" mb="1rem" justify="space-between">
-          <Flex flexDirection="column" ml="1rem">
+        <Flex 
+          justify="space-between" 
+          borderRight="1px solid #FFBA08" 
+          borderBottom="1px solid #FFBA08" 
+          borderLeft="1px solid #FFBA08" 
+          borderRadius="0 0 5px 5px"
+        >
+          <Flex flexDirection="column" ml="1rem" mt="1rem" mb="1rem">
             <Text fontWeight="600" fontSize="1.25rem">Roma</Text>
             <Text mt="0.5rem">Itália</Text>
           </Flex>
           <Image src="bandeiras/italia.svg" mr="1rem"/>
         </Flex>
       </Box>
-      <Box border="1px solid #FFBA08" borderRadius="5px">
+      <Box>
         <Image src="cidades/praga.png" w="100%"/>
-        <Flex mt="1rem" mb="1rem" justify="space-between">
-          <Flex flexDirection="column" mr="3rem" ml="1rem">
+        <Flex 
+          justify="space-between" 
+          borderRight="1px solid #FFBA08" 
+          borderBottom="1px solid #FFBA08" 
+          borderLeft="1px solid #FFBA08" 
+          borderRadius="0 0 5px 5px"
+        >
+          <Flex flexDirection="column" mr="3rem" ml="1rem" mt="1rem" mb="1rem">
             <Text fontWeight="600" fontSize="1.25rem">Praga</Text>
             <Text mt="0.5rem">República Tcheca</Text>
           </Flex>
           <Image src="bandeiras/republicatcheca.svg" mr="1rem"/>
         </Flex>
       </Box>
-      <Box border="1px solid #FFBA08" borderRadius="5px">
+      <Box>
         <Image src="cidades/amsterda.png" w="100%"/>
-        <Flex mt="1rem" mb="1rem" justify="space-between">
-          <Flex flexDirection="column" mr="6rem" ml="1rem">
+        <Flex 
+          justify="space-between" 
+          borderRight="1px solid #FFBA08" 
+          borderBottom="1px solid #FFBA08" 
+          borderLeft="1px solid #FFBA08" 
+          borderRadius="0 0 5px 5px"
+        >
+          <Flex flexDirection="column" mr="6rem" ml="1rem" mt="1rem" mb="1rem">
             <Text fontWeight="600" fontSize="1.25rem">Amsterdã</Text>
             <Text mt="0.5rem">Holanda</Text>
           </Flex>
