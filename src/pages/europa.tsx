d92ec8e7f9925bad463f91b5dbfd83140c85cc7e
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-05-13 14:06:13 -0300

    refactor: Mudanças na pagina

---
+++ src/pages/europa.tsx
@@ -0,0 +1,29 @@
+import { Box, Flex, Image } from "@chakra-ui/react";
+
+import { BannerEuropa } from "../components/BannerEuropa";
+import { Cidades100 } from "../components/Cidades100";
+import { EuropaInfo } from "../components/EurpaInfo";
+import { Header } from "../components/Header";
+
+export default function Continents() {
+  return (
+    <>
+      <Flex
+        w="100%" 
+        h="100%"
+        flexDirection="column" 
+        align="center"
+      >
+        <Header />
+        
+        <Box as="a" href="/" position="absolute" left="100" top="5">
+          <Image src="/voltar.svg"/>
+        </Box>
+
+        <BannerEuropa />
+        <EuropaInfo />
+        <Cidades100 />
+      </Flex>
+    </>
+  )
+}
